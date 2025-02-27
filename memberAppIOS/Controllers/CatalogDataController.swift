774d7a8f430647937bdb89398243b61ef71486d9
SunnyFlops <arav.prasad@outlook.com>
2025-02-27 19:07:22 +1300

    placeholder

---
+++ memberAppIOS/Controllers/CatalogDataController.swift
@@ -0,0 +1,8 @@
+//
+//  CatalogDataController.swift
+//  memberAppIOS
+//
+//  Created by Arav Prasad on 27/02/2025.
+//
+
+import Foundation
---
+++ memberAppIOS/Controllers/SystemDataController.swift
@@ -0,0 +1,8 @@
+//
+//  SystemDataController.swift
+//  memberAppIOS
+//
+//  Created by Arav Prasad on 27/02/2025.
+//
+
+import Foundation
